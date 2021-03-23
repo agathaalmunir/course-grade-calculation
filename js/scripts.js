3c6f48c73bfa1a00706d320c1a807a0000367dc8
Agatha <Agathaalmunir@gmail.com>
2021-03-23 17:51:03 -0400

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
 /************** PART 1 **************/
 
 // STEP 1: Write a variable that will store the total points possible (2500 points) that you can get in this course.
-
+const totalPoints = 2500;
 
 // STEP 2: Use a prompt to collect the number of points a student currently has in the course and store that number in a variable.
-
+let currentPoints = Number(prompt("How many points do you currently have?"));
 
 // STEP 3: Write an arrow function that accepts two arguments. The function should calculate and return the student's CURRENT grade percentage.
 
+let currentGrade = (currentPoints,totalPoints) =>  Math.round((currentPoints/totalPoints) * 100);
+console.log(currentGrade(currentPoints,totalPoints));
 
 // STEP 4: Write on the page a sentence that will say, "Your current grade is [CURRENT GRADE PERCENTAGE]%."
-
+document.write(`Your current grade is ${currentGrade(currentPoints,totalPoints)}%.`);
 
 
 /************** PART 2 **************/
 
 // STEP 5: Use a prompt to collect the grade that the student would like to achieve. The prompt should state, "What is the grade you want? 90, 80, 70 or 60?" Store that number in a variable.
-
+let idealGrade = Number(prompt("What is the grade you want? 90, 80, 70 or 60?"));
 
 // STEP 6: Write an arrow function that accepts THREE arguments. This function will calculate and return the number of ADDITIONAL points the student needs to receive a specified grade. For example, if I tell the function that I have 1200 points and want to know how many points it will take to get a 90% in the course, the function will return 1050.
+
 
 
 // STEP 7: Write a switch statement that will write on the page a sentence. The cases depends on what the student has entered for the grade that they want. The results for each case should say, "You have [CURRENT POINTS] points. You need [ADDITIONAL POINTS NEEDED] points to earn a [GRADE WANTED]% in this course." 
